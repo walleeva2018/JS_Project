2868ecae828fb823080bb85646097b160cc02a54
Zubairrafi <zubair@appscode.com>
2023-05-19 11:33:12 +0600

    mostly working

--- app.js
+++ app.js
@@ -10,8 +10,108 @@ const currentList = [];
 allButton.style.background = 'blue';
 let item = 0;
 
+
 // function
 addButton.addEventListener('click', function () {
+     updateList();
+});
+
+inputField.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+       updateList();
+  }
+});
+
+activeButton.addEventListener('click', function () {
+  allButton.style.background = 'black';
+  doneButton.style.background = 'black';
+  activeButton.style.background = 'blue';
+  
+  clearList();
+  console.log(list)
+
+  debugger
+  for (let i = 0; i < currentList.length; i++) {
+    if (currentList[i].done == undefined) {
+      list.append(currentList[i]);
+    }
+  }
+});
+
+allButton.addEventListener('click', function () {
+  allButton.style.background = 'blue';
+  doneButton.style.background = 'black';
+  activeButton.style.background = 'black';
+
+  clearList();
+
+  for (let i = 0; i < currentList.length; i++) {
+    list.append(currentList[i]);
+  }
+});
+
+doneButton.addEventListener('click', function () {
+  allButton.style.background = 'black';
+  doneButton.style.background = 'blue';
+  activeButton.style.background = 'black';
+   
+  clearList();
+
+  for (let i = 0; i < currentList.length; i++) {
+    if (currentList[i].done == true) {
+      list.append(currentList[i]);
+    }
+  }
+});
+
+clearButton.addEventListener('click', function () {
+   
+  clearList();
+
+  for (let i = 0; i < currentList.length; i++) {
+    if (currentList[i].done == true) {
+      const index = currentList.indexOf(currentList[i]);
+      if (index !== -1) {
+        currentList.splice(index, 1);
+      }
+      i--;
+    }
+    if(currentList[i]!=undefined)
+    list.append(currentList[i]);
+  }
+  currentState();
+});
+
+function currentState() {
+  if (allButton.style.background == 'blue') {
+    allButton.click();
+  }
+  if (doneButton.style.background == 'blue') {
+    doneButton.click();
+  }
+  if (activeButton.style.background == 'blue') {
+    console.log('Came here');
+    activeButton.click();
+  }
+}
+
+function showItem() {
+  if (item > 1) number.innerHTML = `${item} items left`;
+  else if(item==1) number.innerHTML = `${item} item left`;
+  else number.innerHTML="";
+}
+
+function updateList(){
+
+
+  if(inputField.value=="")
+  {
+    return;
+  }
+  const trashIcon = document.createElement("i");
+  trashIcon.classList.add("fas", "fa-trash");
+  const doneIcon = document.createElement("i");
+  doneIcon.classList.add("fas", "fa-check-circle");
   item = item + 1;
   showItem();
   const list = document.getElementById('list');
@@ -21,6 +121,8 @@ addButton.addEventListener('click', function () {
   itemText.innerText = inputField.value;
   itemText.style.color = 'black';
   itemText.style.background = 'white';
+  itemText.style.marginLeft='30px';
+  itemText.style.marginRight='30px';
   inputField.value = '';
 
   const doneButton = document.createElement('button');
@@ -35,10 +137,12 @@ addButton.addEventListener('click', function () {
     itemText.contentEditable = true;
     itemText.focus();
   });
-
+  doneButton.innerHTML="";
+  doneButton.appendChild(doneIcon);
   doneButton.addEventListener('click', function () {
-    item--;
-    if (item > 0) {
+    if(listItem.done!=true)
+    {
+      item--;
       showItem();
     }
     listItem.done = true;
@@ -46,8 +150,14 @@ addButton.addEventListener('click', function () {
     listItem.style.background = 'grey';
     currentState();
   });
-
+  deleteButton.innerHTML="";
+  deleteButton.appendChild(trashIcon);
   deleteButton.addEventListener('click', function () {
+    if(listItem.done!=true)
+    {
+      item--;
+      showItem();
+    }
     listItem.done = false;
     for (let i = 0; i < currentList.length; i++) {
       if (currentList[i] == listItem) {
@@ -69,161 +179,11 @@ addButton.addEventListener('click', function () {
 
   currentList.push(listItem);
   list.appendChild(listItem);
-});
-
-inputField.addEventListener('keydown', function (event) {
-  if (event.key === 'Enter') {
-    item = item + 1;
-    showItem();
-    const list = document.getElementById('list');
-    const listItem = document.createElement('li');
-
-    const itemText = document.createElement('span');
-    itemText.innerText = inputField.value;
-    itemText.style.color = 'black';
-    itemText.style.background = 'white';
-    inputField.value = '';
-
-    const doneButton = document.createElement('button');
-    doneButton.innerText = 'Done';
-    doneButton.style.color = 'green';
-
-    const deleteButton = document.createElement('button');
-    deleteButton.innerText = 'Delete';
-    deleteButton.style.color = 'red';
-
-    itemText.addEventListener('dblclick', function () {
-      itemText.contentEditable = true;
-      itemText.focus();
-    });
-
-    doneButton.addEventListener('click', function () {
-      item--;
-      if (item > 0) {
-        showItem();
-      }
-      listItem.done = true;
-      listItem.style.textDecoration = 'line-through';
-      listItem.style.background = 'grey';
-      currentState();
-    });
-
-    deleteButton.addEventListener('click', function () {
-      listItem.done = false;
-      for (let i = 0; i < currentList.length; i++) {
-        if (currentList[i] == listItem) {
-          const index = currentList.indexOf(currentList[i]);
-
-          if (index !== -1) {
-            currentList.splice(index, 1);
-          }
-          break;
-        }
-      }
-      listItem.remove();
-      currentState();
-    });
-
-    listItem.append(doneButton);
-    listItem.append(itemText);
-    listItem.append(deleteButton);
-
-    currentList.push(listItem);
-    list.appendChild(listItem);
-  }
-});
-
-activeButton.addEventListener('click', function () {
-  allButton.style.background = 'black';
-  doneButton.style.background = 'black';
-  activeButton.style.background = 'blue';
-
-  const list = document.getElementById('list');
-  const itemList = document.querySelectorAll('ul');
-  itemList.forEach((ul) => {
-    while (ul.firstChild) {
-      ul.firstChild.remove();
-    }
-  });
-  for (let i = 0; i < currentList.length; i++) {
-    if (currentList[i].done == undefined) {
-      list.append(currentList[i]);
-    }
-  }
-});
-
-allButton.addEventListener('click', function () {
-  allButton.style.background = 'blue';
-  doneButton.style.background = 'black';
-  activeButton.style.background = 'black';
-
-  const list = document.getElementById('list');
-  const itemList = document.querySelectorAll('ul');
-  itemList.forEach((ul) => {
-    while (ul.firstChild) {
-      ul.firstChild.remove();
-    }
-  });
-
-  for (let i = 0; i < currentList.length; i++) {
-    list.append(currentList[i]);
-  }
-});
-
-doneButton.addEventListener('click', function () {
-  allButton.style.background = 'black';
-  doneButton.style.background = 'blue';
-  activeButton.style.background = 'black';
-
-  const list = document.getElementById('list');
-  const itemList = document.querySelectorAll('ul');
-  itemList.forEach((ul) => {
-    while (ul.firstChild) {
-      ul.firstChild.remove();
-    }
-  });
-  for (let i = 0; i < currentList.length; i++) {
-    if (currentList[i].done == true) {
-      list.append(currentList[i]);
-    }
-  }
-});
-
-clearButton.addEventListener('click', function () {
-  const list = document.getElementById('list');
-  const itemList = document.querySelectorAll('ul');
-  itemList.forEach((ul) => {
-    while (ul.firstChild) {
-      ul.firstChild.remove();
-    }
-  });
-
-  for (let i = 0; i < currentList.length; i++) {
-    if (currentList[i].done == true) {
-      const index = currentList.indexOf(currentList[i]);
-      if (index !== -1) {
-        currentList.splice(index, 1);
-      }
-      i--;
-    }
-    list.append(currentList[i]);
-  }
-});
-
-function currentState() {
-  if (allButton.style.background == 'blue') {
-    allButton.click();
-  }
-  if (doneButton.style.background == 'blue') {
-    doneButton.click();
-  }
-  if (activeButton.style.background == 'blue') {
-    console.log('Came here');
-    activeButton.click();
-  }
+  currentState();
 }
 
-function showItem() {
-  if (item > 1) number.innerHTML = `${item} items left`;
-  else number.innerHTML = `${item} item left`;
+function clearList(){
+  const list = document.getElementById('list');
+  list.innerHTML = '';
 }
+
